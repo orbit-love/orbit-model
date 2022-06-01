65319f38f0b7576c0f9eb74fbf372d43540ef0d3
alexlsalt <am@alexmortoncreative.com>
2022-06-01 12:08:21 +0200

    Added back tracking script to Head

--- components/head.js
+++ components/head.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 
 export default function _Head() {
   return (
@@ -14,11 +15,11 @@ export default function _Head() {
         <meta name="twitter:card" content="summary_large_image" />
         <meta
           property="og:image"
-          content="https://orbitmodel.com/om_social_share.jpg"
+          content="https://orbitmodel.com/images/social-share.jpg"
         />
         <meta
           name="twitter:image"
-          content="https://orbitmodel.com/om_social_share.jpg"
+          content="https://orbitmodel.com/images/social-share.jpg"
         />
         <meta property="og:image:alt" content="Page image for Orbit Model" />
         <meta name="twitter:image:alt" content="Page image for Orbit Model" />
@@ -35,6 +36,20 @@ export default function _Head() {
           content="A framework for building high gravity communities."
         />
       </Head>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          !function(){
+            var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://events.orbit.love/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="yGYwqp3KZ4c6R0uIGIJGA0D6pbZbRecm";;analytics.SNIPPET_VERSION="4.15.3";
+            analytics.load("yGYwqp3KZ4c6R0uIGIJGA0D6pbZbRecm");
+            analytics.page();
+          }}();
+          `}
+      </Script>
+      <script
+        src="https://dog.orbit.love/script.js"
+        data-site="XEYVCUXY"
+        defer
+      ></script>
     </>
   );
 }
